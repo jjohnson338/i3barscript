f53bad46e85e2978cc4ba8b3f4e988b07a7332b0
Jared Johnson <jaredj@gmx.com>
2016-11-15 18:56:49 -0500

    Read up on the actual i3 interface and correctly implement it now

--- index.js
+++ index.js
@@ -22,9 +22,39 @@ const FindAndOutputStats = function() {
 							musicValue,
 						 ]) => {
 		//Format and output results
-		console.log(`Cpu: ${cpuUsageValue}% | Mem: ${memUsageValue} | Disk: ${diskUsageValue} | Music: ${musicValue} | ${timeValue}`);
+		const outputArray = [];
+
+		if (musicValue) {
+			outputArray.push({
+				"full_text": musicValue,
+				"color": "#9be52a",
+				"align": "left"
+			});
+		}
+		outputArray.push({
+			"full_text": `CPU: ${("0"+cpuUsageValue).slice(-5)}%`,
+			"min_width": "CPU: 100%",
+			"align": "left"
+		});
+		outputArray.push({
+			"full_text": `Mem: ${memUsageValue}`,
+			"min_width": "Mem: 16.78GB/16.78GB",
+			"align": "left"
+		});
+		outputArray.push({
+			"full_text": `Disk: ${diskUsageValue}`,
+			"min_width": "Disk: 936.01GB/936.01GB",
+			"align": "left"
+		});
+		outputArray.push({
+			"full_text": timeValue,
+			"align": "right",
+		});
+		console.log(`,${JSON.stringify(outputArray)}`);
 		setTimeout(FindAndOutputStats, 1000);
 	});
 }
-
+console.log(JSON.stringify({ "version": 1 }));
+console.log('[');
+console.log('[]');
 FindAndOutputStats();
