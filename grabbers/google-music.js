25ff8a623af004699c3bca916ebe2d7811b826c0
Jared Johnson <jaredj@gmx.com>
2016-11-15 18:56:23 -0500

    Converted google music grabber to return a promise

--- grabbers/google-music.js
+++ grabbers/google-music.js
@@ -1,14 +1,20 @@
 const execSync = require('child_process').execSync;
 
 module.exports = function getGooglePlayMusic() {
-    const googleMusicObject = JSON.parse(execSync(`cat ~/.config/'Google Play Music Desktop Player'/json_store/playback.json`));
-    if (!googleMusicObject.playing) {
-        return "";
-    } else {
-        const millisecondsPlayed = googleMusicObject.time.current;
-        const totalMilliseconds = googleMusicObject.time.total;
-        const tenthsOfSongPlayed = Math.round(((millisecondsPlayed/totalMilliseconds)*10));
-        const percentageBar = `<${Array(tenthsOfSongPlayed+1).join("#")}${Array((10-tenthsOfSongPlayed)+1).join("-")}>`
-        return `${percentageBar} ${googleMusicObject.song.title} - ${googleMusicObject.song.artist}`;
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const googleMusicObject = JSON.parse(execSync(`cat ~/.config/'Google Play Music Desktop Player'/json_store/playback.json`));
+            if (!googleMusicObject.playing) {
+                resolve(void(0));
+            } else {
+                const millisecondsPlayed = googleMusicObject.time.current;
+                const totalMilliseconds = googleMusicObject.time.total;
+                const tenthsOfSongPlayed = Math.round(((millisecondsPlayed/totalMilliseconds)*10));
+                const percentageBar = `[${Array(tenthsOfSongPlayed+1).join(">")}${Array((10-tenthsOfSongPlayed)+1).join("#")}]`
+                resolve(`${percentageBar} ${googleMusicObject.song.title} - ${googleMusicObject.song.artist}`);
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
 };
