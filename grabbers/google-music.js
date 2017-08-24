d4e6a0ff973ba5c800bda8ee4269ced338d0abd7
Jared Johnson <jaredj@gmx.com>
2017-08-23 21:34:46 -0400

    Now uses GPMDP's websocket interface instead of querying the filesystem

--- grabbers/google-music.js
+++ grabbers/google-music.js
@@ -1,20 +1,70 @@
-const execSync = require('child_process').execSync;
+const WebSocket = require('ws');
 
-module.exports = function getGooglePlayMusic() {
-    return new Promise((resolve, reject) => {
-        try {
-            const googleMusicObject = JSON.parse(execSync(`cat ~/.config/'Google Play Music Desktop Player'/json_store/playback.json`));
-            if (!googleMusicObject.playing) {
-                resolve(void(0));
-            } else {
-                const millisecondsPlayed = googleMusicObject.time.current;
-                const totalMilliseconds = googleMusicObject.time.total;
-                const tenthsOfSongPlayed = Math.round(((millisecondsPlayed/totalMilliseconds)*10));
-                const percentageBar = `[${Array(tenthsOfSongPlayed+1).join(">")}${Array((10-tenthsOfSongPlayed)+1).join("#")}]`
-                resolve(`${percentageBar} ${googleMusicObject.song.title} - ${googleMusicObject.song.artist}`);
-            }
-        } catch (e) {
-            resolve(void(0));
-        }
-    });
-};
+module.exports = (function getGooglePlayMusic() {
+  //Constantly updated object containing music state
+  const state = {
+    playing: false,
+    artist: null,
+    track: null,
+    milliSecondsPlayed: null,
+    milliSecondsTotal: null,
+    albumArtworkUrl: null,
+  };
+ 
+  let gpmdpWS = null; 
+  
+  function tryLater() {
+    gpmdpWS = null;
+    state.playing = false;
+    setTimeout(tryConnect, 500);
+  }
+
+  function tryConnect() {
+    try {
+      //Create the websocket
+      gpmdpWS = new WebSocket('ws://localhost:5672');
+      gpmdpWS.on('error', (e) => {
+        tryLater();
+      });
+      gpmdpWS.on('open', () => {
+        //Setup listeners
+        gpmdpWS.on('message', (payload) => {
+          //Payload comes as stringified JSON. Parse
+          payload = JSON.parse(payload);
+          //Payload is of the structure { channel: "channelName", payload: payload }
+          const channel = payload.channel;
+          if (channel == "playState") {
+            state.playing = payload.payload;
+          } else if (channel == "track") {
+            const { title, artist, album, albumArt } = payload.payload;
+            state.artist = artist;
+            state.track = title;
+            state.albumArtworkUrl = albumArt;
+          } else if (channel == "time") {
+            const { current, total } = payload.payload;
+            state.milliSecondsPlayed = current;
+            state.milliSecondsTotal = total;
+          }
+        });
+        gpmdpWS.on('close', tryLater);
+      }); 
+    } catch(e) {
+      tryLater();
+    }
+  }
+
+  tryConnect();
+
+  const getMusicBar = () => {
+    if (!state.playing) {
+      return;
+    } else {
+      const tenthsOfSongPlayed = Math.round(((state.milliSecondsPlayed/state.milliSecondsTotal)*10));
+      const percentageBar = `[${Array(tenthsOfSongPlayed+1).join(">")}${Array((10-tenthsOfSongPlayed)+1).join("#")}]`
+      return `${percentageBar} ${state.track} - ${state.artist}`;
+    }
+  };
+  return {
+    getMusicBar,
+  };
+})();
