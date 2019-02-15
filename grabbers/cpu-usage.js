a2cd76291e1d2ed6fe2221e17d6ee65644e87679
Jared Johnson <jaredj@gmx.com>
2019-02-14 22:48:58 -0500

    Use top for better CPU usage polling

--- grabbers/cpu-usage.js
+++ grabbers/cpu-usage.js
@@ -1,52 +1,29 @@
-//Thanks to https://gist.github.com/bag-man/5570809
+const execSync = require("child_process").execSync;
+const formatBuffer = require("../utils/utils");
 
-const os = require("os");
+const smoothingLength = 50;
+let smoothingArray = [];
 
-//Create function to get CPU information
-const cpuAverage = () => {
-
-  //Initialise sum of idle and time of cores and fetch CPU info
-  let totalIdle = 0, totalTick = 0;
-  const cpus = os.cpus();
-
-  //Loop through CPU cores
-  for(let i = 0, len = cpus.length; i < len; i++) {
-
-    //Select CPU core
-    const cpu = cpus[i];
-
-    //Total up the time in the cores tick
-    for(type in cpu.times) {
-      totalTick += cpu.times[type];
-   }     
-
-    //Total up the idle time of the core
-    totalIdle += cpu.times.idle;
-  }
-
-  //Return the average Idle and Tick times
-  return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
+const avgArray = (arr) => {
+    return Math.round(arr.reduce(
+      (acc, cur) => {
+        return acc + cur;
+      },
+      0
+    ) / arr.length);
 };
 
-module.exports = (async () => {
-  const startMeasure = cpuAverage();
-  const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  };
+module.exports = async function getCPUUsage(){
+  const cpuLoad = 100 - parseInt(formatBuffer(
+    execSync(`top -bn 1 | grep "id," | awk '{print $8}'`)
+  ));
 
-  const collectCpuLoad = async () => { 
-    await sleep(100);
-    //Grab second Measure
-    const endMeasure = cpuAverage(); 
-
-    //Calculate the difference in idle and total time between the measures
-    const idleDifference = endMeasure.idle - startMeasure.idle;
-    const totalDifference = endMeasure.total - startMeasure.total;
-
-    //Calculate the average percentage CPU usage
-    const percentageCPU = 100 - ~~(100 * idleDifference / totalDifference);
-
-    return percentageCPU.toFixed(2);
-  };
-  return await collectCpuLoad();
-});
+  if (smoothingArray.length < smoothingLength) {
+    smoothingArray.push(cpuLoad);
+  } else {
+    smoothingArray = [ cpuLoad ].concat(
+      smoothingArray.slice((smoothingLength-1))
+    );
+  }
+  return avgArray(smoothingArray);
+};
